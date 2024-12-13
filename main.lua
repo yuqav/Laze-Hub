4dae8ad84ca2da9f2de95bb4c355b93cc8d46c49
yuqav <tronyax@gmail.com>
2024-12-13 18:59:26 +0300

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,26 @@
+local githubURL = "https://raw.githubusercontent.com/yuqav/Laze-Hub/refs/heads/main/scripts/"
+
+local scripts = {
+ [116605585218149] = "gofishing.lua"
+}
+
+local placeId = game.PlaceId
+
+local function ScriptFound(placeId)
+ return scripts[placeId] ~= nil
+end
+
+local function RunScript(placeId)
+ if not ScriptFound(placeId) then
+         warn("Script not found: " .. placeId)
+  return
+     end
+
+ local filename = scripts[placeId]
+ local scriptURL = githubURL .. filename
+ local script = game:HttpGet(scriptURL)
+ _G.keyFound = true
+ return loadstring(script)()
+end
+
+RunScript(placeId)
